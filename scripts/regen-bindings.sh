d48a33af4c245ef4c0f48feac5e6c0f550f66634
Woodrow Douglass <wdouglass@carnegierobotics.com>
2025-12-16 15:10:23 -0500

    Regenerate bindings for openh264 2.6.0

---
+++ scripts/regen-bindings.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Using Bindgen 0.71.1
+
+export HEADER=../openh264-sys2/upstream/codec/api/wels/codec_api.h
+
+bindgen \
+    ${HEADER} \
+    --generate-block \
+    --no-layout-tests \
+    --no-prepend-enum-name \
+    --with-derive-eq --with-derive-default --with-derive-hash --with-derive-ord --generate-cstr \
+    --use-array-pointers-in-arguments \
+    --generate types \
+    -o ../openh264-sys2/src/generated/types.rs
+
+bindgen \
+    ${HEADER} \
+    --generate-block \
+    --no-layout-tests \
+    --no-prepend-enum-name \
+    --with-derive-eq --with-derive-default --with-derive-hash --with-derive-ord \
+    --use-array-pointers-in-arguments \
+    --generate vars \
+    -o ../openh264-sys2/src/generated/consts.rs
+
+bindgen \
+    ${HEADER} \
+    --generate-block \
+    --no-layout-tests \
+    --no-prepend-enum-name \
+    --merge-extern-blocks \
+    --wrap-unsafe-ops \
+    --with-derive-eq --with-derive-default --with-derive-hash --with-derive-ord \
+    --use-array-pointers-in-arguments \
+    --raw-line "use super::types::*;" \
+    --generate functions \
+    -o ../openh264-sys2/src/generated/fns_source.rs
+
+bindgen \
+    ${HEADER} \
+    --generate-block \
+    --no-layout-tests \
+    --no-prepend-enum-name \
+    --merge-extern-blocks \
+    --wrap-unsafe-ops \
+    --with-derive-eq --with-derive-default --with-derive-hash --with-derive-ord \
+    --use-array-pointers-in-arguments \
+    --raw-line "use super::types::*;" \
+    --dynamic-loading APILoader \
+    --generate functions \
+    -o ../openh264-sys2/src/generated/fns_libloading.rs
